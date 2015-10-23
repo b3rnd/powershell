547e99d86305db4bfd977511ce12366430ac95db
Bernd <b3rnd@users.noreply.github.com>
2015-10-23 08:59:05 +0200

    code review

--- truphone_datausage.ps1
+++ truphone_datausage.ps1
@@ -8,46 +8,28 @@ do { Start-Sleep -m 100 } while ( $ie.ReadyState -ne 4 )
 $ie.Document.getElementById("Username").value = "your_username"
 $ie.Document.getElementById("Password").value = "your_password"
 
-do { Start-Sleep -m 2000 } while ( $ie.ReadyState -ne 4 )
+do { Start-Sleep -m 5000 } while ( $ie.ReadyState -ne 4 )
 
 $ie.Document.getElementById("loginSubmit").click()
 
-do { Start-Sleep -m 2000 } while ( $ie.ReadyState -ne 4 )
+do { Start-Sleep -m 5000 } while ( $ie.ReadyState -ne 4 )
 
 $ie.Document.getElementById("loginSubmit").click()
 
-do { Start-Sleep -m 2000 } while ( $ie.ReadyState -ne 4 )
+do { Start-Sleep -m 5000 } while ( $ie.ReadyState -ne 4 )
 
-$body = $ie.Document.body | Out-String
+$body = "dataUsage: " + $ie.Document.getElementById("dataUsage").outertext.trim() + "`n" + "`n" + "`n"
+$body = $body + "freeMinutes: "+ $ie.Document.getElementById("freeMinutes").outertext.trim() + "`n" + "`n" + "`n"
+$body = $body + "inbundleMinutes: "+ $ie.Document.getElementById("inbundleMinutes").outertext.trim() + "`n" + "`n" + "`n"
+$body = $body + "textsSent: "+ $ie.Document.getElementById("textsSent").outertext.trim() + "`n" + "`n" + "`n"
 
-$array = $body.Split("`n")
+$EmailFrom = "x@y.z" #Summary Email Sender Address
+$EmailTo = "x@y.z" #Summary Email Receiver
+$EmailSubject = "Truphone Status" #Summary Email Subject
+$SMTPServer = "x.x.x.x" #Mail Server Address
 
-foreach ($line in $array)
-{
-	if ($line.contains("text:dataUsedWithFormat -->"))
-	{
-		$j = 7
-		for($i = 58; $i -le 70; $i++)
-		{
-			if($line.Substring(58,7) -eq "<")
-			{
-				$j = $i - 57
-				break
-			}
-		}
-		
-		$usage = $line.Substring(58,$j)
-		
-		$EmailFrom = "x@y.z" #Summary Email Sender Address
-		$EmailTo = "x@y.z" #Summary Email Receiver
-		$EmailSubject = "Truphone Datenverbrauch: " + $usage #Summary Email Subject
-		$SMTPServer = "x.x.x.x" #Mail Server Address
-		
-		Send-MailMessage -SmtpServer $SMTPServer -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Body $usage
-		
-		break
-	}
-}
+$body
+Send-MailMessage -SmtpServer $SMTPServer -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Body $body
 
 $ie.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie) | out-null
